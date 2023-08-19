638c0f52ea38433b5fa6b2a71546dad508e64271
MikaelMagal <mikaelmagalhaes28@gmail.com>
2023-08-19 19:46:31 -0300

    criacao do bd/api

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from bd import mysql
+from flaskext.mysql import MySQL
+from flask import Flask, render_template, request, redirect, url_for  
+
+app = Flask(__name__)      
+
+
+
+if(__name__ == "__main__"):
+    app.run(debug = True)
