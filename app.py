7ec1650fb2ca49024e65c81fc536048bed246046
MikaelMagal <mikaelmagalhaes28@gmail.com>
2023-08-22 01:12:24 -0300

    fix: error js

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from bd import mysql
+from models.user import Usuario
 from flaskext.mysql import MySQL
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__, static_url_path='/assets', static_folder='assets')   
+app = Flask(__name__, static_url_path='/assets', static_folder='assets')
+
+user_global = Usuario()
+email_global = ""
 
 #instanciar
 @app.route('/')                                                     #Criando o Rout
 def homepage():                                                     #Criando a Função
-    return render_template("index.html")
+    return render_template("login.html")
 
 @app.route('/Login')                                                #Criando o Rout
 def Login():                                                #Criando a Função
@@ -33,8 +37,8 @@ def adicionar_usuario():
             with mysql.cursor() as cur:
                 cur.execute('INSERT INTO Usuario (nome, email, senha, cpf) VALUES (%s,%s,%s,%s)', (nome, email, senha, cpf))
                 cur.connection.commit()
-
-                return redirect(url_for('usuarios', nome_usuario=nome))
+                
+                return render_template("login.html")
         else:
             return render_template("create.html")
 
@@ -52,17 +56,30 @@ def verificar_usuario():
                 comando = "SELECT email, nome, senha FROM Usuario WHERE	email = %s;"
                 cur.execute(comando, email)
                 resultado = cur.fetchall()
-                print(resultado)
                 if(resultado[0][0] != None):
                     if(resultado[0][2] != senha):
                         return render_template("login.html")
                     else:
-                        return redirect(url_for('usuarios', nome_usuario=resultado[0][1]))
+                        email_global = resultado[0][1]
+                        return render_template("index.html")
 
         except Exception as e:
             return render_template("login.html")
 
+@app.route('/getUserByEmail', methods = ['GET'])
+def getUserByEmail():
+    args = request.args.get('email')
+    try:
+        with mysql.cursor() as cur:
+            comando = "SELECT * FROM Usuario WHERE	email = %s;"
+            cur.execute(comando, args)
+            resultado = cur.fetchall()
+            user_global.cria(resultado[0][0], resultado[0][1], resultado[0][2], resultado[0][3])
+            return 
 
+    except Exception as e:
+        print("Deu ruim")
+        return render_template("login.html")
 
 if(__name__ == "__main__"):
     app.run(debug = True)
---
+++ models/user.py
@@ -0,0 +1,11 @@
+class Usuario:
+        
+    def __init__(self):
+        pass
+    
+    def cria(self, nome, email, senha, cpf):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.cpf = cpf
+        
