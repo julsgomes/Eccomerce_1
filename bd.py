959edd8bb8c6df61e918163d0b0ec24b6905a752
Julsgomes <juliagomes1676@gmail.com>
2023-08-23 17:44:11 -0300

    finilização

--- bd.py
+++ bd.py
@@ -4,6 +4,6 @@ mysql = pymysql.connect(
     host='localhost',
     port=3306,
     user='root',
-    passwd='Mikael2012',
-    database='PrimeStore'
+    passwd='Ju1031&68',
+    database='bd_soft'
 )
